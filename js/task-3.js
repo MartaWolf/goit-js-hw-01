42d45a398155029df60c807500b511dd8d5fad2e
Marta Wolf <martawolfb@gmail.com>
2026-02-28 13:31:36 +0100

    add all file

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+function getElementWidth(content, padding, border) {
+  const contentWidth = parseFloat(content);
+  const contentPadding = parseFloat(padding);
+  const contentBorder = parseFloat(border);
+  return contentWidth + contentPadding * 2 + contentBorder * 2;
+}
+
+console.log(getElementWidth('50px', '8px', '4px')); // 74
+console.log(getElementWidth('60px', '12px', '8.5px')); // 101
+console.log(getElementWidth('200px', '0px', '0px')); // 200
